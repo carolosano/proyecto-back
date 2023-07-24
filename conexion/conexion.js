c0aae2cf679279b2c30c5b3d1b4ecee5059a0d4f
Caro <losanocarolina2@gmail.com>
2023-07-24 10:05:30 -0300

    deploy back

---
+++ conexion/conexion.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+//Traemos las url de conexiones a Mongo
+const MONGOLOCAL = process.env.MONGOLOCAL;
+//const MONGOATLAS = process.env.MONGOATLAS;
+
+//Conexion a la base de datos Mongo - 2
+mongoose.connect(MONGOLOCAL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => {
+        console.log(`Database conectada a local`);
+    })
+    .catch((error) => {
+        console.log(`El error es: ${error}`)
+    })
